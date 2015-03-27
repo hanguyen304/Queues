1047b846fe76b2d84646e2fbb939f0a37fecb2c9
Ha Nguyen <hanguyen304@users.noreply.github.com>
2015-03-26 22:17:27 -0400

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -3,30 +3,38 @@ var express = require('express')
 var request = require('request')
 var proxy = express()
 var client = redis.createClient(6379, '127.0.0.1', {})
-var nexthit='3000'
+var nextserver='3000'
 
 var proxy_server = proxy.listen(2015, function () {
 
 	  var host = proxy_server.address().address
 	  var port = proxy_server.address().port
-	client.set('lasthit','3000')
+	client.set('lastserver','3000')
    	console.log('Example app listening at http://%s:%s', host, port)
 })
 
-proxy.get('/', function(req, res) {
-		  client.get('lasthit',function(err,value){
-				if(value == '3000'){
-					nexthit='3001'
-					client.set('lasthit','3001')
-				}
-				else{
-					nexthit='3000'
-					client.set('lasthit','3000')
-				}
-				request('http://localhost:'+nexthit+'/', function (error, response, body) {
-					if (!error && response.statusCode == 200) {
-						res.send(body) 
-					}
-				})
-			})
+proxy.get('/', function(req, res) 
+{
+	client.get('lastserver',function(err,value)
+	{
+		if(value == '3000')
+		{
+			nextserver='3001'
+			client.set('lastserver','3001')
+		}
+		else
+		{
+			nextserver='3000'
+			client.set('lastserver','3000')
+		}
+		request('http://localhost:'+nextserver, function (error, response, body) 
+		{
+			if (!error && response.statusCode == 200) 
+			{
+				res.send(body) 
+			}
+
+		})
+	});
+	client.get("lastserver",function(err,value){console.log("Last server: "+value)});
 })
